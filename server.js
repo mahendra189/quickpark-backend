4f16abfacd88519ec3b4117b48104e84643d7928
mahendra189 <mahendrank18082004@gmail.com>
2025-06-27 16:32:02 +0530

    done with setup basic

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const { Pool } = require("pg");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // required by Render
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM users");
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+app.get("/", (req, res) => {
+  res.send("Hello World! Node JS server is running.");
+});
+
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
